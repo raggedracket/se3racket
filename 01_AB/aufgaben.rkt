c20f4baff48f73c50ad105d51a3c902c3b136e1b
otisjuliusson <4juliuss@informatik.uni-hamburg.de>
2015-10-21 09:55:45 +0200

    Fixed 2.1

--- 01_AB/aufgaben.rkt
+++ 01_AB/aufgaben.rkt
@@ -7,9 +7,12 @@
 (define (RADtoDEGR number)
   (radians->degrees number))
 
+(define (sintocos cosa)
+  (sqrt(- 1 (expt cosa 2))))
+
 ;alpha ist winkel der funktion
-(define (my-acos alpha)
-  (atan (/ (sin alpha) (cos alpha))))
+(define (my-acos cosa)
+  (atan (/ (sintocos cosa) cosa)))
 
 (define(nm->km nm)
   (* nm 1.852))
@@ -18,4 +21,7 @@
 
 
 (define (distanzAB breiteA laengeA breiteB laengeB)
-  ((RADtoDEGR(+(*(sin(DEGRtoRAD breiteA))(sin(DEGRtoRAD breiteB)))(*(cos(DEGRtoRAD breiteA))(cos(DEGRtoRAD breiteB))(cos(-(DEGRtoRAD laengeA)(DEGRtoRAD laengeB))))))))
+  (* 60 (RADtoDEGR(my-acos (+(*(sin(DEGRtoRAD breiteA))(sin(DEGRtoRAD breiteB)))(*(cos(DEGRtoRAD breiteA))(cos(DEGRtoRAD breiteB))(cos(-(DEGRtoRAD laengeA)(DEGRtoRAD laengeB)))))))))
+;(distanzAB 59.93 10.75 22.20 114.10)
+;(distanzAB 37.75 -122.45 21.32 -157.83)
+;(distanzAB -27.10 -109.40 -12.10 -77.05)
