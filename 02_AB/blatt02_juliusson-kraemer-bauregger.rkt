6fa65c41adcf06507edb52a7141222377f549ceb
Johnderate <jannis@4kraemer.me>
2015-10-28 21:29:45 +0100

    Evaluationen Aufgabe 1 eingetragen

--- 02_AB/blatt02_juliusson-kraemer-bauregger.rkt
+++ 02_AB/blatt02_juliusson-kraemer-bauregger.rkt
@@ -19,32 +19,46 @@
 (define xs3 (cons katze miau))
 
 ;# 1.01 miau
+;'Plueschi
 ;
 ;# 1.02 katze
+;'plueschi
 ;
 ;# 1.03 tiger
+;'miau
 ;
 ;# 1.04 (quote katze)
+;'katze
 ;
 ;# 1.05 (eval tiger)
+;'Plueschi
 ;
 ;# 1.06 (eval katze)
+; Error Plueschi: undefined
 ;
 ;# 1.07 (eval 'tiger)
+;’miau
 ;
 ;# 1.08 (welcherNameGiltWo 'harry 'potter)
+;'harry
 ;
 ;# 1.09 (cdddr xs1)
+;'(miau katze)
 ;
 ;# 1.10 (cdr xs2)
+;'(Plueschi)
 ;
 ;# 1.11 (cdr xs3)
+;'Plueschi
 ;
 ;# 1.12 (eval (sqrt 3))
+;1.7320508075688772
 ;
 ;# 1.13 (eval '(welcherNameGiltWo 'tiger 'katze))
+;'tiger
 ;
 ;# 1.13 (eval (welcherNameGiltWo 'katze 'tiger))
+;’Plueschi
 ;
 
 ;##### Aufgabe 2 - #####
