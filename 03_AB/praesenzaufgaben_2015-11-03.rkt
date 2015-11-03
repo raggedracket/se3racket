3d11a846e5ea58466b91876722c4931d2dc46ea8
Maximilian Bauregger <4bauregg@informatik.uni-hamburg.de>
2015-11-03 11:48:19 +0100

    2. Praesenzaufgabe

--- 03_AB/praesenzaufgaben_2015-11-03.rkt
+++ 03_AB/praesenzaufgaben_2015-11-03.rkt
@@ -5,3 +5,12 @@
     [(empty? xs) 0]
     [(equal? (car xs) element) (+ 1 (anzahl (cdr xs) element))]
     [(anzahl (cdr xs) element)]))
+
+(define (translate element xs)
+    (cond
+      [(empty? xs) 'NotDefined]
+      [(equal? (caar xs) element) (cdar xs)]
+      [(translate element (cdr xs))]
+))
+
+;#(assoc )
