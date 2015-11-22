1ee93334eb72164b2ca77a8bfd957d2bed60303b
Maximilian Bauregger <4bauregg@informatik.uni-hamburg.de>
2015-11-22 23:20:11 +0100

    [+] 04_AB, 05_AB, 06_AB

---
+++ 05_AB/blatt05_juliusson-kraemer-bauregger.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+; Blatt 05
+;
+; Otis Juliusson, Jannis Krämer, Maximilian Bauregger
+; Raum F-534 - Di, 10:15 - Finn Günther
+
+;##### Aufgabe 1 - #####
+
+;### 1.1 ###
+;# 1.1.1
+
+;##### Aufgabe 2 - #####
+
+;### 2.1 ###
+
