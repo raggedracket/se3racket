d8af508e7345e291f411c5157636736d843c0789
Maximilian Bauregger <max.bauregger@posteo.de>
2016-01-10 22:01:58 +0100

    [10_AB] Add - init

---
+++ 10_AB/blatt10_juliusson-kraemer-bauregger.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+; Otis Juliusson, Jannis Krämer, Maximilian Bauregger
+; Raum F-534 - Di, 10:15 - Finn Günther
+
+;##### Aufgabe 1 #####
+
+(define game #(0 0 0 0 0 9 0 7 0
+               0 0 0 0 8 2 0 5 0
+               3 2 7 0 0 0 0 4 0
+               0 1 6 0 4 0 0 0 0
+               0 5 0 0 0 0 3 0 0
+               0 0 0 0 9 0 7 0 0
+               0 0 0 6 0 0 0 0 5
+               8 0 2 0 0 0 0 0 0
+               0 0 4 2 0 0 0 0 8))
+
+;(define (xy->index x y)
+;  ())
+
+;(define (row->idx row)
+;  ())
+
+;(define (column->idx col)
+;  ())
+
+;(define (quadr->idx quadr)
+;  ())
+
+;(define (game->entries game idx)
+;  ())
+
+;(define (game-consistent? game)
+;  ())
+
+;(define (game-solved? game)
+;  ())
